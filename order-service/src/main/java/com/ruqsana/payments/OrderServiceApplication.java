773e503a9700bd5bdfabea20b574199f9d6ebbf2
Ruqsana <107501777+Ruqsana2504@users.noreply.github.com>
2026-01-26 21:48:52 +0530

    order-service added

---
+++ order-service/src/main/java/com/ruqsana/payments/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ruqsana.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
