340e52cebcfdfc587277f2de661ade7b96623e12
Ruqsana <107501777+Ruqsana2504@users.noreply.github.com>
2026-02-01 03:24:06 +0530

    Added circuit breaker for retries and fallback mechanism

--- inventory-service/src/main/java/com/payment/inventory/service/InventoryService.java
+++ inventory-service/src/main/java/com/payment/inventory/service/InventoryService.java
@@ -1,5 +1,8 @@
 package com.payment.inventory.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,19 +12,35 @@ public class InventoryService {
     @Autowired
     InventoryTxnService txService;
 
-    private static final int MAX_RETRIES = 3;
+//    private static final int MAX_RETRIES = 3;
 
+    @Retry(name = "inventoryRetry")
+    @CircuitBreaker(
+            name = "inventoryCB",
+            fallbackMethod = "reserveFallback"
+    )
+    @Transactional
     public boolean reserveStockWithRetry(String productId, int quantity) {
-        for (int attempts = 1; attempts <= MAX_RETRIES; attempts++) {
-            try {
-                return txService.reserveStock(productId, quantity);
-            } catch (Exception e) {
-                if (attempts == MAX_RETRIES) {
-                    throw e;
-                }
-            }
-        }
+        return txService.reserveStock(productId, quantity);
+    }
+
+    public boolean reserveFallback(String productId, int quantity, Throwable ex) {
+        System.out.println("Inventory service degraded: " + ex.getMessage());
         return false;
     }
+
+
+//    public boolean reserveStockWithRetry(String productId, int quantity) {
+//        for (int attempts = 1; attempts <= MAX_RETRIES; attempts++) {
+//            try {
+//                return txService.reserveStock(productId, quantity);
+//            } catch (Exception e) {
+//                if (attempts == MAX_RETRIES) {
+//                    throw e;
+//                }
+//            }
+//        }
+//        return false;
+//    }
 
 }
