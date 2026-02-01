52d7542bb203d583fb791641b8abe4cd0321a7ef
Ruqsana <107501777+Ruqsana2504@users.noreply.github.com>
2026-02-02 00:19:14 +0530

    Added ApiConfig, Caught exceptions for inventory-service api call

--- order-service/src/main/java/com/payment/order/service/InventoryGateway.java
+++ order-service/src/main/java/com/payment/order/service/InventoryGateway.java
@@ -2,10 +2,12 @@ package com.payment.order.service;
 
 import com.payment.order.dto.InventoryRequest;
 import com.payment.order.dto.OrderRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class InventoryGateway {
 
@@ -16,21 +18,31 @@ public class InventoryGateway {
     }
 
     public String reserve(OrderRequest orderRequest) {
-        ResponseEntity<String> inventoryReserveResponse = restTemplate.postForEntity(
-                "http://inventory-service/api/v1/inventory/reserve",
-                new InventoryRequest(orderRequest.getProductId(), orderRequest.getQuantity()),
-                String.class
-        );
-        return inventoryReserveResponse.getBody();
+        ResponseEntity<String> inventoryReserveResponse = null;
+        try {
+            inventoryReserveResponse = restTemplate.postForEntity(
+                    "http://localhost:8081/inventory/reserve",
+                    new InventoryRequest(orderRequest.getProductId(), orderRequest.getQuantity()),
+                    String.class
+            );
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return null != inventoryReserveResponse ? inventoryReserveResponse.getBody() : null;
     }
 
     public String release(OrderRequest orderRequest) {
-        ResponseEntity<String> inventoryReleaseResponse = restTemplate.postForEntity(
-                "http://inventory-service/api/v1/inventory/release",
-                new InventoryRequest(orderRequest.getProductId(), orderRequest.getQuantity()),
-                String.class
-        );
-        return inventoryReleaseResponse.getBody();
+        ResponseEntity<String> inventoryReleaseResponse = null;
+        try {
+            inventoryReleaseResponse = restTemplate.postForEntity(
+                    "http://localhost:8081/inventory/release",
+                    new InventoryRequest(orderRequest.getProductId(), orderRequest.getQuantity()),
+                    String.class
+            );
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return null != inventoryReleaseResponse ? inventoryReleaseResponse.getBody() : null;
     }
 
 }
